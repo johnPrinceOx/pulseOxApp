902606d0ebd297927baaf06502ee777ce0557719
John Prince <john.prince@eng.ox.ac.uk>
2016-10-09 16:34:26 +0100

    Started to add email methods - need to create new mail activity.

--- Hathi/mainApp/src/main/java/org/pnuemoniaproject/hathi/firstMenu.java
+++ Hathi/mainApp/src/main/java/org/pnuemoniaproject/hathi/firstMenu.java
@@ -7,11 +7,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -19,12 +21,15 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
+import db.utils.GMailSender;
+import db.utils.Mail;
 import db.utils.johnUsefulMethods;
 import devices.RecordActivity;
 
 public class firstMenu extends AppCompatActivity {
 
     private static Button newPatient, oldPatient, quickMeasure;
+    private Mail m;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,40 +64,53 @@ public class firstMenu extends AppCompatActivity {
         quickMeasure.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(firstMenu.this, RecordActivity.class));
-                finish();
+
+
+                /*
+                try {
+                    GMailSender sender = new GMailSender("reports@starstudy.org", "R3p0rt5z");
+                    sender.sendMail("This is Subject",
+                            "This is Body",
+                            "john.prince@eng.ox.ac.uk",
+                            "john.prince.ox@gmail");
+                } catch (Exception e) {
+                    Log.e("SendMail", e.getMessage(), e);
+                }
+                */
+
+                // startActivity(new Intent(firstMenu.this, RecordActivity.class));
+               m = new Mail("reports@starstudy.org", "R3p0rt5z");
+                sendEmail(view);
+
+                //finish();
             }
         });
 
+    }
 
+    public void sendEmail(View view){
+        String[] toArr = {"reports@starstudy.org"}; // This is an array, you can add more emails, just separate them with a coma
+        m.setTo(toArr); // load array to setTo function
+        m.setFrom("reports@starstudy.org"); // who is sending the email
+        m.setSubject("subject");
+        m.setBody("Test \n test tes test tes test");
+
+        try {
+            //m.addAttachment("/sdcard/myPicture.jpg");  // path to file you want to attach
+            if(m.send()) {
+                // success
+                Toast.makeText(firstMenu.this, "Email was sent successfully.", Toast.LENGTH_LONG).show();
+            } else {
+                // failure
+                Toast.makeText(firstMenu.this, "Email was not sent.", Toast.LENGTH_LONG).show();
+            }
+        } catch(Exception e) {
+            // some other problem
+            Toast.makeText(firstMenu.this, "There was a problem sending the email.", Toast.LENGTH_LONG).show();
+        }
 
     }
 
-
-
-
-    /*
-    @Override
-    public void onClick(View v) {
-
-        int id = v.getId();
-        if (id == R.id.createNew) {
-
-            startActivity(new Intent(firstMenu.this, johnactivity.class));
-            finish();
-        }
-        else if (id == R.id.lookOld) {
-
-            //startActivity(new Intent(firstMenu.this, HospiStream2.class));
-            //finish();
-        }
-
-        else if (id == R.id.quick) {
-            startActivity(new Intent(firstMenu.this,NewMeasurement.class));
-        }
-
-    }
-*/
 
 
 
