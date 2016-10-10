c0d5d953f5bf797b354e10130863c47d140d7a77
johnPrinceOx <wolf4510@eng.ox.ac.uk>
2016-10-10 13:19:56 +0100

    Successfully attached and sent a csv as an attachment

--- Hathi/mainApp/src/main/java/org/pnuemoniaproject/hathi/firstMenu.java
+++ Hathi/mainApp/src/main/java/org/pnuemoniaproject/hathi/firstMenu.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -42,6 +43,7 @@ import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 
+import db.CVDVariables;
 import db.utils.GMailSender;
 import db.utils.Mail;
 import db.utils.SendMailTask;
@@ -57,6 +59,11 @@ public class firstMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_menu);
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -87,22 +94,86 @@ public class firstMenu extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+
+
                 Log.i("SendMailActivity", "Send Button Clicked.");
 
                 String fromEmail = "reports@starstudy.org";
                 String fromPassword = "R3p0rt5z";
 
-                List<String> toEmailList = Arrays.asList("john.prince.ox@gmail.com", "elina.naydenova@yahoo.com", "john.prince@eng.ox.ac.uk");
+                List<String> toEmailList = Arrays.asList("john.prince.ox@gmail.com", "john.prince@eng.ox.ac.uk");
                 Log.i("SendMailActivity", "To List: " + toEmailList);
                 String emailSubject = "Test Subject";
-                String emailBody = "Test Content \nIf you get this, then the app is working!";
+                String emailBody = "Test Content \n \n \n If you get this, then the app is working!";
                 new SendMailTask(firstMenu.this).execute(fromEmail,
                         fromPassword, toEmailList, emailSubject, emailBody);
+
+                String preamble = "/storage/emulated/0/";
+                String attachResult = sendEmailAttachment("john.prince@eng.ox.ac.uk", "Test Subject", "Main Text", preamble + "PatientDetailsFinal.csv");
+                Log.i("Attach Result", attachResult);
+                Toast.makeText(firstMenu.this, "Email Attachment: "+ attachResult, Toast.LENGTH_SHORT).show();
 
                 //finish();
             }
         });
 
     }
+
+
+    public static String sendEmailAttachment(String emailTo, String subject, String text, String filepath){
+        //Sending an email with a file attachment
+
+        final String username = "reports@starstudy.org";
+        final String password = "R3p0rt5z";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+
+            Multipart mp = new MimeMultipart();
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("reports@starstudy.org"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(emailTo));
+            message.setSubject(subject);
+
+
+            MimeBodyPart mbp1 = new MimeBodyPart();
+            mbp1.setText(text);
+            mbp1.setContent(text, "text/html");
+            mp.addBodyPart(mbp1);
+
+            MimeBodyPart mbp = new MimeBodyPart();
+            FileDataSource fds = new FileDataSource(filepath);
+            mbp.setDataHandler(new DataHandler(fds));
+            mbp.setFileName(fds.getName());
+            mp.addBodyPart(mbp);
+
+            message.setContent(mp);
+
+            Transport.send(message);
+            return "Email Sent";
+
+        } catch (MessagingException e) {
+
+            return "Error sending email: "+ e.toString();
+            //			throw new RuntimeException(e);
+        }
+    }
+
+
 }
 
